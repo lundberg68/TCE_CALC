1c8006f8c01870643fdc162adc4203bf7e797cb2
dtran65 <dtran65@users.noreply.github.com>
2015-09-24 17:31:49 -0400

    Update Calculator.cpp

--- cppfiles/Calculator.cpp
+++ cppfiles/Calculator.cpp
@@ -6,14 +6,14 @@
  */
 #include "../include/Calculator.h"
 
-float CalculatorInAction(float x, float y)
+float CalculatorInAction(float x, float y, const char calcOperation)
 {
 	char brks, meto;
 	char opr;
 	Calculator myCal(1.0,2.0);
 
+	opr = calcOperation;
 	// Perform Add Operation
-	opr = '+';
 	return myCal.getCalcResult(x, y, opr);
 
 } // End of Cal In Action
@@ -24,8 +24,8 @@ int main()
 
 	Calculator();					// Object Constructor
 	Calculator(0.0, 0.0);
-	Calculator myCal(1.0, 2.0);
-	myValue = CalculatorInAction(3.0,4.0);
+	Calculator myCal(1.0, 2.0, '*');
+	myValue = CalculatorInAction(3.0,4.0,'+');
 	Calculator(myValue,myValue);
 
 	return 0;
